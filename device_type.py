17abefa3197c6b8466e9b654eca7006fc9bc67db
slashix <72565138+slashix@users.noreply.github.com>
2023-01-24 12:30:25 +0300

    Update device_type.py

--- device_type.py
+++ device_type.py
@@ -1,33 +1,37 @@
 from netmiko import ConnectHandler
 from netmiko import NetMikoTimeoutException, SSHException
 
-def determine_device_type(ip, username, password, method='ssh'):
+def determine_device_type(ip, username, password):
+    """
+    Function to determine the type of network device
+    :param ip: IP address of the device
+    :param username: username for login
+    :param password: password for login
+    :return: type of device ('cisco_ios', 'eltex', 'huawei') or error message
+    """
+    device = {
+        'device_type': 'cisco_ios',
+        'ip': ip,
+        'username': username,
+        'password': password,
+    }
     try:
-        device = {
-            'device_type': 'cisco_ios',
-            'ip': ip,
-            'username': username,
-            'password': password,
-            'method': method,
-        }
         net_connect = ConnectHandler(**device)
-        prompt = net_connect.find_prompt()
-        version = net_connect.send_command("show version")
-        if '>' in prompt and '<' in prompt:
-            device_type = 'huawei'
-        elif '>' in prompt or '#' in prompt:
-            if "Cisco" in version:
-                device_type = 'cisco_ios'
-            else:
-                device_type = 'eltex'
+    except (NetMikoTimeoutException, SSHException):
+        device['device_type'] = 'cisco'
+        net_connect = ConnectHandler(**device)
+    prompt = net_connect.find_prompt()
+    version = net_connect.send_command("show version")
+    if '>' in prompt and '<' in prompt:
+        device_type = 'huawei'
+    elif '>' in prompt or '#' in prompt:
+        if "Cisco" in version:
+            device_type = 'cisco_ios'
         else:
-            device_type = 'unknown'
-        return device_type
-    except (NetMikoTimeoutException, SSHException) as e:
-        if method == 'ssh':
-            return determine_device_type(ip, username, password, method='telnet')
-        else:
-            return f"Error: {e}"
+            device_type = 'eltex'
+    else:
+        device_type = 'unknown'
+    return device_type
 
 # Example of usage
 ip = '192.168.1.1'
