dfc2fe9298412b5b6d1d2203430e75a18af4017e
slashix <72565138+slashix@users.noreply.github.com>
2023-02-08 10:30:50 +0300

    Create vlan_list.py

---
+++ vlan_list.py
@@ -0,0 +1,21 @@
+import netmiko
+
+device = {
+    "device_type": "cisco_nxos",
+    "ip": "192.168.1.1",
+    "username": "admin",
+    "password": "secret",
+    "secret": "secret",
+}
+
+connection = netmiko.ConnectHandler(**device)
+
+vlan_list = connection.send_command("show vlan brief")
+vlan_list = vlan_list.split("\n")
+vlan_list = [vlan.split() for vlan in vlan_list[3:-1]]
+
+with open("vlan_list.txt", "w") as f:
+    for vlan in vlan_list:
+        f.write(";".join(vlan) + "\n")
+
+connection.disconnect()
