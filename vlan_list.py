357cd4ba6663baf9dd8e406670d6b13391928c48
slashix <72565138+slashix@users.noreply.github.com>
2023-02-08 10:37:12 +0300

    Update vlan_list.py

--- vlan_list.py
+++ vlan_list.py
@@ -12,7 +12,7 @@ connection = netmiko.ConnectHandler(**device)
 
 vlan_list = connection.send_command("show vlan brief")
 vlan_list = vlan_list.split("\n")
-vlan_list = [vlan.split() for vlan in vlan_list[3:-1]]
+vlan_list = [vlan.split()[:2] for vlan in vlan_list[3:-1]]
 
 with open("vlan_list.txt", "w") as f:
     for vlan in vlan_list:
