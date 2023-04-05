ae4638f89f3bc0b0817a098f3ece24b7f4e0a9a6
slashix <72565138+slashix@users.noreply.github.com>
2023-04-05 12:38:41 +0300

    Update progress_bar.py

--- progress_bar.py
+++ progress_bar.py
@@ -19,3 +19,32 @@ def send_config_commands(device, command_list):
             pbar.close()
     except Exception as error:
         print(error)
+        
+==================================
+import math
+from netmiko import ConnectHandler
+from tqdm import tqdm
+
+def send_config_commands(device, command_list):
+    try:
+        with ConnectHandler(**device) as ssh:
+            ssh.enable()
+
+            # Инициализируем прогресс-бар
+            pbar = tqdm(total=len(command_list))
+
+            # Разбиваем список команд на блоки размером в 500 строк
+            chunk_size = 500
+            chunks = [command_list[i:i+chunk_size] for i in range(0, len(command_list), chunk_size)]
+
+            # Выполняем каждый блок команд
+            for chunk in chunks:
+                ssh.send_config_set(chunk, delay_factor=2)
+                # Обновляем прогресс-бар
+                pbar.update(len(chunk))
+
+            # Закрываем прогресс-бар
+            pbar.close()
+    except Exception as error:
+        print(error)
+
